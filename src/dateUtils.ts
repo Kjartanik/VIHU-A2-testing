9ef5587de3fa44686651af0dd5072fd2253d4df2
KjartanIK <kjartanik22@ru.is>
2025-02-09 21:43:14 +0000

    Update: Types added to dateUtils.ts

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,22 +1,22 @@
-import moment from "moment";
+import moment, { MomentInput } from "moment";
 import { DATE_UNIT_TYPES } from "./constants";
 
-export function getCurrentYear() {
+export function getCurrentYear(): number{
   return moment().year();
 }
 
-export function add(date, number, type = DATE_UNIT_TYPES.DAYS) {
+export function add(date: MomentInput, number: number, type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS): Date {
   return moment(date).add(number, type).toDate();
 }
 
-export function isWithinRange(date, from, to) {
+export function isWithinRange(date: MomentInput, from: MomentInput, to: MomentInput): boolean {
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date, compareDate) {
+export function isDateBefore(date: MomentInput, compareDate: MomentInput): boolean {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date, compareDate) {
+export function isSameDay(date: MomentInput, compareDate: MomentInput): boolean {
   return moment(date).isSame(compareDate);
 }
